971dc130c5156043ff70763adfbc6ea14fa6eb89
Chris Forstall <cforstall@gmail.com>
2018-06-14 12:40:07 -0300

    remove numpy-based code, create framework for Alex's work

--- bin/make_samples.py
+++ bin/make_samples.py
@@ -19,9 +19,8 @@ from mta_summer_2018 import Config, Text
 
 def sample(lines, sample_size, offset=0):
     '''group lines in to paragraphs'''
-    
-    samples = np.split(lines, np.arange(offset, lines.size, sample_size))
-    return [' '.join(s) for s in samples]
+    # TODO
+    pass
 
 #
 # main
@@ -52,10 +51,4 @@ if __name__ == '__main__':
     for text in corpus:
         text.dataFromJson(os.path.join(args.dest, text.author + '.json'))
         
-        print('{} {}: {} lines'.format(text.author, text.title, len(text.lines)))
-        
-        s = sample(text.lines, 50)
-        
-        print('--> {} samples'.format(len(s)))
-        print()
-        
+        # TODO : call sample
